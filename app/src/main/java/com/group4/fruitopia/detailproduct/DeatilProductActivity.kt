6aea2680d1c80a9acc5b7afe059560a99084a8e2
panipujayanti <panipujayanti@gmail.com>
2024-06-18 07:40:03 +0700

    add detail product activity

---
+++ app/src/main/java/com/group4/fruitopia/detailproduct/DeatilProductActivity.kt
@@ -0,0 +1,21 @@
+package com.group4.fruitopia.detailproduct
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.group4.fruitopia.R
+
+class DeatilProductActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_deatil_product)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
